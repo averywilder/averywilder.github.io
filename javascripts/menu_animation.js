c87fa5e14681677890c9142858da7219252ef63b
vrywldr <vrywldr@gmail.com>
2017-11-23 22:03:18 -0500

    navigation test

--- javascripts/menu_animation.js
+++ javascripts/menu_animation.js
@@ -1,4 +1,18 @@
-$(document).ready(function(){
-  
-  
-}
+(function () {
+
+    var app = angular.module('sampleApp',['ngRoute']);
+
+    app.config(function ($routeProvider){
+        $routeProvider
+            .when('/',{
+                templateUrl:'art.html'
+            })
+            .when('/about',{
+                templateUrl:'about.html'
+            })
+            .when('/contact',{
+              templateUrl:'contact.html'
+            })
+            .otherwise({ redirectTo:'/'});
+    });
+})();

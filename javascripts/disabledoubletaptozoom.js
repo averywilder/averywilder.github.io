16344711cc7d21797550a42c5b9b39623d4800d6
vrywldr <vrywldr@gmail.com>
2018-02-02 21:57:01 -0500

    test

--- javascripts/disabledoubletaptozoom.js
+++
@@ -1,7 +0,0 @@
-$(document).click(function(event) {
-    element = document.elementFromPoint(event.clientX, event.clientY);
-    if(document.getElementById("div_excluded_from_doubletap").contains(element)) {
-        event.preventDefault();
-        clickFunction(element);
-    }
-});
